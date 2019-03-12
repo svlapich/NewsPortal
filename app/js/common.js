9f799cffbb5b95ffddcc759c56a4ffc90661bbdb
MikalaiKazak <garryroach6456@gmail.com>
2019-03-13 00:45:22 +0300

    add transitin on the new page

--- app/js/common.js
+++ app/js/common.js
@@ -1,38 +1,33 @@
-var regFindHastTag = /\B\#\w\w+\b/g;
+var regFindHashTag = /\B\#\w\w+\b/g;
 var regFindEmail = /@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])/g;
 var findFirstSent = /[^\.\!\?]+[\.\!\?]/;
 
-
-$(document).ready(function() {
-  onStart(getPosts);
-});
-
+onStart(getData);
 
 //function for get response from vk
-function getPosts(data) {
-  var hashTag, postCode, postTitle, postText, attachment, photoSize;
+function getData(data) {
+  var hashTag, postCode, postTitle, postText, postType, postImage;
   var posts = data.response.items;
   for (var i in posts) {
     var post = posts[i];
-    hastTag = post.text.match(regFindHastTag);
-    postCode = post.text.match(regFindEmail);
-    postText = post.text.replace(hastTag, '').replace(postCode, '');
-    postTitle = postText.match(findFirstSent);
-    if (post.attachments[0]['photo']) {
-      attachment = 'photo';
-      photoSize = 'photo_604';
-    } else if (post.attachments[0]['video']) {
-      attachment = 'video';
-      photoSize = 'photo_640';
-    } else if (post.attachments[0]['link']) {
-      attachment = 'link';
-      photoSize = 'photo_640' //TODO doesnt work
+
+    postType = post.attachments[0].type;
+    hashTag = post.text.match(regFindHashTag)[0];
+    postCode = post.text.match(regFindEmail)[0];
+    postText = post.text.replace(hashTag, '').replace(postCode, '');
+    postTitle = postText.match(findFirstSent)[0];
+
+    if (postType == 'photo') {
+      postImage = post.attachments[0]['photo']['photo_604'];
+    } else if (postType == 'video') {
+      postImage = post.attachments[0]['video']['photo_640'];
+    } else if (postType == 'link') {
+      postImage = post.attachments[0]['link']['photo']['photo_604'];
     }
-    console.log(post);
-    drawPosts(post.attachments[0][attachment][photoSize], 
-      postTitle, postText, hashTag, postCode);
-  }
+    drawPosts(postImage, postTitle, postText, hashTag, postCode);
+  };
 }
+
 
 function getUrl(method, params) {
   if (!method) throw Error('There is incorrect method');
@@ -42,30 +37,55 @@ function getUrl(method, params) {
 }
 
 function drawPosts(postImage, postTitle, postText, hashTag, postCode) {
-  var htmlFields = '';
-  htmlFields +=
-    '<div class="mdl-grid mdl-cell mdl-cell--12-col-desktop mdl-cell--9-col-tablet mdl-cell--4-col-phone mdl-card mdl-shadow--4dp">\n' +
+  var card = document.createElement('div');
+  card.className = "mdl-grid mdl-cell mdl-cell--12-col-desktop mdl-cell--9-col-tablet mdl-cell--4-col-phone mdl-card mdl-shadow--4dp";
+  card.innerHTML =
     ' <div class="mdl-cell  mdl-cell--4-col-desktop mdl-cell--hide-tablet mdl-cell--4-col-phone">\n' +
     '   <img class="card_image" src="' + postImage + '" alt="">\n' +
     ' </div>\n' +
     '   <div class="mdl-cell mdl-cell--8-col">\n' +
-    '   <h2 class="mdl-card__title-text mdl-card--expand">' + postTitle + '</h2>\n' +
+    '   <h2 style="margin-right: 30px;" class="mdl-card__title-text mdl-card--expand">' + postTitle + '</h2>\n' +
     '         <div class="mdl-card__menu mdl-cell--hide-phone">\n' +
     '             <button class="mdl-button mdl-button--icon mdl-js-button mdl-js-ripple-effect">\n' +
     '               <i class="material-icons">share</i></button>\n' +
     '           </div>\n' +
     '           <div class="mdl-card__supporting-text no-left-padding">\n' +
-    '             <p>' + postText + '...' + '</p>\n' +
+    '             <p>' + postText.slice(0, 400) + '...' + '</p>\n' +
     '           </div>\n' +
     '           <div class="mdl-card__subtitle-text">\n' +
-    '             <span>Category: <a href="#">' + hastTag + '</a></span>\n' +
+    '             <span>Category: <a href="#">' + hashTag + '</a></span>\n' +
     '           </div>\n' +
     '           <div style="text-align: right;" class="mdl-card__actions mdl-card--border">\n' +
-    '       <a class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect">Read more</a>\n' +
+    '       <button id="read-more" class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect">Read more</button\n' +
     '           </div>\n' +
+    ' </div>\n';
+  card.addEventListener('click', function() {
+    displayPostOnNewPage(postImage, postTitle, postText, hashTag);
+  }, false);
+  var o = document.getElementById("content");
+  o.appendChild(card);
+}
+
+function displayPostOnNewPage(postImage, postTitle, postText, hashTag) {
+  var postWindow = window.open("post.html");
+  var newArticle =
+    ' <div class="article_header">\n' +
+    '   <h1>' + postTitle + '</h1>\n' +
     ' </div>\n' +
-    '</div>';
-  $('#content').append(htmlFields);
+    ' <div class="article__content">\n' +
+    '   <div class="article_image__wrapper">\n' +
+    '     <img class="article_image" src="' + postImage + '" alt="">\n' +
+    '   </div>\n' +
+    '   <div class="article_text">' + postText + '</div>\n' +
+    '   <div class="article_tags">\n' +
+    '     <span>Category: <a href="#">' + hashTag + '</a></span>\n' +
+    '   </div>\n' +
+    '   <div class="article_like"></div>\n' +
+    ' </div>\n'
+  postWindow.onload = function() {
+    postWindow.document.getElementById("full-post").innerHTML = newArticle;
+  }
+  postWindow.focus()
 }
 
 var isLoaded = false;
